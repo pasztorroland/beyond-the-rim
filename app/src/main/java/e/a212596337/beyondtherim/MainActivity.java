7946e33361b561a9c27d6af6f938e41e4186260d
Pasztor <212596337@logon.ds.ge.com>
2018-01-02 21:39:17 +0100

    first commit

---
+++ app/src/main/java/e/a212596337/beyondtherim/MainActivity.java
@@ -0,0 +1,13 @@
+package e.a212596337.beyondtherim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
